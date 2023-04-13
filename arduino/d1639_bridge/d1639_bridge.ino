46e48f9c5151ff37418c1748704494271cfdfd98
Roman Dobrodii <ztcoils@gmail.com>
2023-04-14 01:09:43 +0200

    First working proof-of-concept

--- arduino/d1639_bridge/d1639_bridge.ino
+++ arduino/d1639_bridge/d1639_bridge.ino
@@ -1,221 +1,323 @@
-// Tx channel
-const int pin_data_o = A2;
-const int pin_rdy_i = A1;
-// Rx channel
-const int pin_data_i = A0;
-const int pin_rdy_o = A3;
-// LED
-const int pin_led = 13;
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-// Timing constants
-#define T_BIT_US (100u)
-#define T_1HBIT_US (150u)
+/*
+ * Pinout:
+ * 
+ * DATA_I - io4 - PD4 (ICP1)
+ * RDY_I  - io5 - PC6
+ * DATA_O - io6 - PD7
+ * RDY_O  - io7 - PE6
+ * io13 - LED
+ */
+
+#define P_BIT_DATA_I    (4u)
+#define P_BIT_RDY_I     (6u)
+#define P_BIT_DATA_O    (7u)
+#define P_BIT_RDY_O     (6u)
+
+#define P_DDR_DATA_I    DDRD, P_BIT_DATA_I
+#define P_DDR_RDY_I     DDRC, P_BIT_RDY_I
+#define P_DDR_DATA_O    DDRD, P_BIT_DATA_O
+#define P_DDR_RDY_O     DDRE, P_BIT_RDY_O
+
+#define P_PIN_DATA_I    PIND, P_BIT_DATA_I
+#define P_PIN_RDY_I     PINC, P_BIT_RDY_I
+
+#define P_PORT_DATA_I   PORTD, P_BIT_DATA_I
+#define P_PORT_RDY_I    PORTC, P_BIT_RDY_I
+#define P_PORT_DATA_O   PORTD, P_BIT_DATA_O
+#define P_PORT_RDY_O    PORTE, P_BIT_RDY_O
+
+const uint8_t pin_led = 13;
+
+/**
+ * Timing constraints
+ */
+#define T_BIT_US        (100u)
+#define T_1BIT_US       (150u)
+
+/**
+ * Timer 1 timing conversions
+ */
+#define T1_OSC_DIV      (8u)
+#define T1_TICKS(us)    ((u16) ((u32)F_CPU / 1000000 * (us) / T1_OSC_DIV))
+
+const u16 t1_ticks_bit = T1_TICKS(T_BIT_US);
+const u16 t1_ticks_1bit = T1_TICKS(T_1BIT_US);
+const u16 t1_ticks_tx_dly = T1_TICKS(50);
 
 
+/**
+ * RAII IRQ lock implementation for critical sections
+ */
+class LockIRQ
+{
+ u8 _sreg;
+public:
+  LockIRQ() : _sreg(SREG)
+  {
+    cli();
+  }
+  ~LockIRQ()
+  {
+    SREG = _sreg;
+  }
+};
+
+/**
+ * Helper bit ops
+ */
+#define BITCLR_(v, n) \
+  do { \
+    (v) = (v) & ~(1u << (n)); \
+  } while(0)
+
+#define BITSET_(v, n) \
+  do { \
+    (v) = (v) | (1u << (n)); \
+  } while(0)
+
+#define BITWR_(v, n, x) \
+  do { \
+    (v) = ((v) & ~(1u << (n))) | (((x) ? 1 : 0) << (n)); \
+  } while(0)
+
+#define BITRD_(v, n) (((v) >> (n)) & 1)
+
+#define BITCLR(...) BITCLR_(__VA_ARGS__)
+#define BITSET(...) BITSET_(__VA_ARGS__)
+#define BITWR(...) BITWR_(__VA_ARGS__)
+#define BITRD(...) BITRD_(__VA_ARGS__)
+
+#define BARRIER() do { asm volatile("":::"memory"); } while(0)
+
+/**
+ * Tx state machine
+ */
+enum {
+  TX_IDLE,
+  TX_WAIT_RDY,
+  TX_XFER,
+};
+
+u8 tx_state = TX_IDLE;
+u8 tx_busy;
+u8 tx_data;
+u8 tx_biti;
+
+/**
+ * Tx: FSM logic
+ * 
+ * Called from main(), this code is not time-critical
+ */
+void fsm_tx() {
+  if (tx_state == TX_IDLE) {
+    if (Serial.available()) {
+      tx_state = TX_WAIT_RDY;
+      tx_data = Serial.read();
+      // set DATA_O = 0
+      BITCLR(P_PORT_DATA_O);
+    }
+  } else if (tx_state == TX_WAIT_RDY) {
+    if (BITRD(P_PIN_RDY_I) == 0) {
+      tx_state = TX_XFER;
+      // Kickoff timer-driven machinery in 200 us (empirical "safe" value)
+      tx_biti = 8;
+      tx_busy = 1;
+      {
+        LockIRQ lock_;
+        OCR1A = TCNT1 + t1_ticks_tx_dly;
+        BITSET(TIFR1, OCF1A);
+        BARRIER();
+        BITSET(TIMSK1, OCIE1A); 
+      }
+    }
+  } else {
+    if (!tx_busy) {
+      // Timer finished its job, disabled its interrupt, set DATA_O to 1
+      // we just return to IDLE
+      tx_state = TX_IDLE;
+    }
+  }
+}
+
+/**
+ * Tx: Timer 1 Compare A IRQ handler
+ * 
+ * Fires at scheduled intervals to write data bit value to DATA_O
+ */
+ISR(TIMER1_COMPA_vect) {
+  /*
+   * tx_biti:
+   *  - 8 in the _beginning_ of a start bit
+   *  - 7 in the _beginning_ of 7th bit (MSB)
+   *  ...
+   *  - 0 in the _beginning_ of 0th bit (LSB)
+   *    and we still must keep on the line for 100us!
+   *  - 255 after 0th bit transmission has been finished
+   *  - 254 after guard bit transmission has been finished
+   *  
+   *  We transmit guard bit because otherwise, if some unexpected
+   *  latency gets mixed in and last bit gets prolonged, the
+   *  receiver (MHB8748) may treat our last bit as a beginning of
+   *  a new frame. To avoid this, we explicitly transmit '1' - a
+   *  guard bit - after LSB.
+   */
+  if (tx_biti == 254) {
+    // end of transfer -> set DATA_O to 1, disable interrupt
+    BITSET(P_PORT_DATA_O);
+    BITCLR(TIMSK1, OCIE1A);
+    BARRIER();
+    tx_busy = 0;
+  } else {
+    if (tx_biti <= 7) {
+      // transmit data bit
+      BITWR(P_PORT_DATA_O, tx_data & 0x80);
+      tx_data <<= 1;
+    } else {
+      // transmit start or guard bit
+      BITSET(P_PORT_DATA_O);
+    }
+    // schedule next interrupt
+    OCR1A += t1_ticks_bit;
+    tx_biti--;
+  }
+}
+
+
+/**
+ * Rx state machine
+ */
+enum {
+  RX_IDLE,
+  RX_XFER,
+};
+
+u8 rx_state = RX_IDLE;
+u8 rx_busy;
+u8 rx_data;
+u8 rx_biti;
+
+/**
+ * Rx: FSM logic
+ * 
+ * Called from main(), this code is not time-critical
+ */
+void fsm_rx() {
+  if (rx_state == RX_IDLE) {
+    if (BITRD(P_PIN_DATA_I) == 0) {
+      rx_state = RX_XFER;
+      rx_busy = 1;
+      // Device has data for us -> enable DATA_I Input Capture IRQ
+      {
+        LockIRQ lock_;
+        BITSET(TIFR1, ICF1);
+        BARRIER();
+        BITSET(TIMSK1, ICIE1);
+        // Signal RDY and expect Input Capture interrupt
+        BITCLR(P_PORT_RDY_O);
+      }
+    }
+  } else {
+    if (!rx_busy) {
+      // Timer finished its job, disabled its interrupt, removed RDY,
+      // so we send data to host and return to idle
+      if (Serial.availableForWrite()) {
+        BARRIER();
+        Serial.write(rx_data);
+        rx_state = RX_IDLE;
+      }
+    }
+  }
+}
+
+/**
+ * Rx - Timer 1 Input Capture handler
+ * 
+ * Fires when ICP1 (DATA_I) detects rising edge of a start bit
+ */
+ISR(TIMER1_CAPT_vect) {
+  // Disable IC interrupt, clear RDY signal
+  // schedule and enable OC-B interrupt
+  BITCLR(TIMSK1, ICIE1);
+  BITSET(P_PORT_RDY_O);
+  OCR1B = ICR1 + t1_ticks_1bit;
+  BARRIER();
+  BITSET(TIFR1, OCF1B);
+  BITSET(TIMSK1, OCIE1B);
+  rx_biti = 7;
+  rx_data = 0;
+}
+
+/**
+ * Rx - Timer 1 Compare B handler
+ * 
+ * Fires at scheduled intervals to read data bit value from DATA_I
+ */
+ISR(TIMER1_COMPB_vect) {
+  /*
+   * NOTE: we can't just bail out after we've received last bit (LSB)
+   * that is because LSB value will still be driven on the data line for
+   * ~50us and that is enough to trigger our Rx state machine into
+   * perceiving it as a beginning of a new frame. To avoid that, we
+   * "receive" yet another bit after LSB and ignore it.
+   */
+  if (rx_biti == 255) {
+    // end of transfer -> disable interrupt
+    BITCLR(TIMSK1, OCIE1B);
+    BARRIER();
+    rx_busy = 0;
+  } else {
+    if (rx_biti <= 7) {
+      // Read data bit
+      rx_data = (rx_data << 1) | BITRD(P_PIN_DATA_I);
+    }
+    // schedule next interrupt
+    OCR1B += t1_ticks_bit;
+    rx_biti--;
+  }
+}
+
+/*
+ * setup()
+ */
 void setup() {
-  // Init GPIO
-  pinMode(pin_data_o, OUTPUT);
-  pinMode(pin_rdy_i, INPUT_PULLUP);
-  pinMode(pin_data_i, INPUT_PULLUP);
-  pinMode(pin_rdy_o, OUTPUT);
+  // GPIO
+  BITWR(P_DDR_DATA_O, 0);
+  BITWR(P_DDR_RDY_O, 0);
+  BITWR(P_DDR_DATA_I, 0);
+  BITWR(P_DDR_RDY_I, 0);
+  //
+  BITWR(P_PORT_DATA_I, 1);
+  BITWR(P_PORT_RDY_I, 1);
+  BITWR(P_PORT_DATA_O, 1);
+  BITWR(P_PORT_RDY_O, 1);
+  //
+  BITWR(P_DDR_DATA_O, 1);
+  BITWR(P_DDR_RDY_O, 1);
+
   pinMode(pin_led, OUTPUT);
+
+  // Timer1 - f=f_osc/8, Input Compare on posedge, counter starts
+  TCCR1A = 0;
+  TCCR1B = (1<<ICES1) | (1<<CS11);
+  
   // Serial
   Serial.begin(115200);
+
+  // Enable interrupts
+  sei();
 }
 
-//
-// TX FSM
-//
-
-enum {
-  FSM_TX_IDLE,
-  FSM_TX_WAIT_RDY,
-  FSM_TX_XFER,
-};
-
-uint8_t tx_state = FSM_TX_IDLE;
-uint8_t tx_data;
-uint8_t tx_biti;
-uint32_t tx_tick;
-
-void fsm_tx(uint32_t t) {
-  // Next variables
-  uint8_t next_state;
-  uint8_t next_data;
-  uint8_t next_biti;
-  uint32_t next_tick;
-  
-  // 'Combinational' logic
-  bool tock = ((int32_t)(t - tx_tick)) >= 0;
-  bool rdy = (digitalRead(pin_rdy_i) == LOW);
-  uint8_t state = tx_state;
-
-  // State change
-  next_state = state;
-  if (state == FSM_TX_IDLE) {
-    if (Serial.available()) {
-      next_state = FSM_TX_WAIT_RDY;
-    }
-  } else if (state == FSM_TX_WAIT_RDY) {
-    if (rdy) {
-      next_state = FSM_TX_XFER;
-    }
-  } else if (state == FSM_TX_XFER) {
-    if (tx_biti == 0 && tock) {
-      next_state = FSM_TX_IDLE;
-    }
-  }
-
-  bool state_change = (next_state != state);
-
-  // Data
-  next_data = tx_data;
-  if (state == FSM_TX_IDLE && state_change) {
-    next_data = Serial.read();
-  }
-
-  // Bit counter
-  next_biti = tx_biti;
-  if (state_change && next_state == FSM_TX_XFER) {
-    next_biti = 8;
-  } else if (state == FSM_TX_XFER && tock) {
-    next_biti--;
-  }
-
-  // Timing
-  if (state_change && next_state == FSM_TX_XFER) {
-    next_tick = t + T_BIT_US;
-  } else if (state == FSM_TX_XFER && tock) {
-    next_tick += T_BIT_US;
-  }
-
-  // Save registered logic states
-  tx_state = next_state;
-  tx_data = next_data;
-  tx_biti = next_biti;
-  tx_tick = next_tick;
-  
-  // Compute combinatorial outputs: DATA_O
-  if (tx_state == FSM_TX_IDLE) {
-    digitalWrite(pin_data_o, HIGH);
-  } else if (tx_state == FSM_TX_WAIT_RDY) {
-    digitalWrite(pin_data_o, LOW);
-  } else if (tx_state == FSM_TX_XFER) {
-    bool v = HIGH;
-    if (tx_biti <= 7) {
-      v = (tx_data >> tx_biti) & 1;
-    }
-    digitalWrite(pin_data_o, v);
-  }
-}
-
-//
-// RX FSM
-//
-
-enum {
-  FSM_RX_IDLE,
-  FSM_RX_WAIT_START,
-  FSM_RX_XFER,
-};
-
-uint8_t rx_state = FSM_RX_IDLE;
-uint8_t rx_data;
-uint8_t rx_biti;
-uint32_t rx_tick;
-
-void fsm_rx(uint32_t t) {
-  // Next variables
-  uint8_t next_state;
-  uint8_t next_data;
-  uint8_t next_biti;
-  uint32_t next_tick;
-  
-  // 'Combinational' logic
-  bool tock = ((int32_t)(t - rx_tick)) >= 0;
-  bool di = digitalRead(pin_data_i);
-  uint8_t state = rx_state;
-
-  // State change
-  next_state = state;
-  if (state == FSM_RX_IDLE) {
-    if (Serial.availableForWrite() && (di == LOW)) {
-      next_state = FSM_RX_WAIT_START;
-    }
-  } else if (state == FSM_RX_WAIT_START) {
-    if (di == HIGH) {
-      next_state = FSM_RX_XFER;
-    }
-  } else if (state == FSM_RX_XFER) {
-    // 1 extra bit interval delay before returning to IDLE
-    if (rx_biti == 255 && tock) {
-      next_state = FSM_RX_IDLE;
-    }
-  }
-
-  bool state_change = (next_state != state);
-
-  // Data
-  next_data = rx_data;
-  if (state == FSM_RX_XFER && tock) {
-    next_data = (next_data << 1) | (di ? 1 : 0);
-  }
-
-  // Bit counter
-  next_biti = rx_biti;
-  if (state_change && next_state == FSM_RX_XFER) {
-    next_biti = 7;
-  } else if (state == FSM_RX_XFER && tock) {
-    next_biti--;
-  }
-
-  // Timing
-  if (state_change && next_state == FSM_RX_XFER) {
-    next_tick = t + T_1HBIT_US;
-  } else if (state == FSM_RX_XFER && tock) {
-    next_tick += T_BIT_US;
-  }
-
-  // Send byte to host after transfer ends
-  if (state_change && state == FSM_RX_XFER) {
-    if(Serial.availableForWrite()) {
-      Serial.write(rx_data);  // must not block
-    } // else signal error?
-  }
-
-  // Save registered logic states
-  rx_state = next_state;
-  rx_data = next_data;
-  rx_biti = next_biti;
-  rx_tick = next_tick;
-  
-  // Compute combinatorial outputs: RDY_O
-  if (rx_state == FSM_RX_WAIT_START) {
-    digitalWrite(pin_rdy_o, LOW);
-  } else {
-    digitalWrite(pin_rdy_o, HIGH);
-  }
-}
-
-bool ll;
-
-
+/**
+ * main() loop
+ */
 void loop() {
-//  uint32_t t = micros();
+  BARRIER();
+  fsm_tx();
+  fsm_rx();
 
-  fsm_tx(micros());
-  fsm_rx(micros());
-
-  { 
-    static int i;
-    i++;
-    if (i >= 20000) {
-      ll = !ll;
-      i = 0;
-    }
-  }
-
-  // Activate LED if either TX or RX is not IDLE  
-//  digitalWrite(pin_led, (rx_state != FSM_RX_IDLE) || (tx_state != FSM_TX_IDLE));
-  digitalWrite(pin_led, ll);
+  // Activate LED if either TX or RX is not IDLE
+  digitalWrite(pin_led, rx_busy || tx_busy);
 }
--- python/adcon.py
+++ python/adcon.py
@@ -1,16 +1,32 @@
 from serial import Serial
+import random
+import logging
+import sys
+import struct
+from typing import Tuple
+
+logger = logging.getLogger('adcon')
 
 
-class Comm:
+class MetraD1639:
+    MEAS_20MS = 0b001
+    MEAS_200MS = 0b010
+    MEAS_2000MS = 0b100
+
+    TEST_COMM = 1
+
     TIMEOUT = 0.5
     TIMEOUT_CONV = 5
 
     def __init__(self, port: str):
-        self.p = Serial(port, timeout=self.TIMEOUT, write_timeout=self.TIMEOUT)
+        self.p = Serial(port)
 
-    def xchg(self, tx: bytes, num_rx: int = 0, rx_timeout: float = -1):
-        print(f'write {tx!r}, read {num_rx} bytes')
-        self.p.write(tx)
+    def xchg(self, tx: bytes = (), num_rx: int = 0, rx_timeout: float = -1):
+        #logger.debug(f'xchg: write {len(tx)} bytes ({tx!r}), read {num_rx} bytes')
+        self.p.write_timeout = self.TIMEOUT
+        self.p.timeout = self.TIMEOUT
+        if tx:
+            self.p.write(tx)
         rx = ()
         if num_rx:
             if rx_timeout < 0:
@@ -22,26 +38,102 @@ class Comm:
         return rx
 
     def set_mode(self, mode: int, fil: bool):
+        logger.debug(f'set_mode: mode={mode}, fil={fil}')
+        mode = int(mode)
         assert 0 <= mode <= 31
-        cmd = (1 if fil else 0) << 5
-        cmd |= mode & 0x1F
-        cmd &= 0xFF
+        cmd = mode & 0x1F
+        if fil:
+            cmd |= 1 << 5
         self.xchg(bytes((cmd,)))
 
+    def run_test(self, test: int):
+        logger.debug(f'run_test: test={test}')
+        test = int(test)
+        assert 1 <= test <= 15
+        cmd = 1 << 7
+        cmd |= (test & 0xF) << 3
+        self.xchg(bytes((cmd,)))
+
+    def run_meas(self, t1_mains_cycles: int) -> Tuple[int, int]:
+        logger.debug(f'run_meas: T1 = {t1_mains_cycles} mains cycles...')
+        if t1_mains_cycles == 1:
+            dur = self.MEAS_20MS
+            timeout = 0.5
+        elif t1_mains_cycles == 10:
+            dur = self.MEAS_200MS
+            timeout = 0.8
+        elif t1_mains_cycles == 100:
+            dur = self.MEAS_2000MS
+            timeout = 4.0
+        else:
+            raise ValueError(f'Unsupported T1 duration: {t1_mains_cycles!r}')
+        cmd = 1 << 7
+        cmd |= dur & 0x7
+        rly = self.xchg(bytes((cmd,)), 5, rx_timeout=timeout)
+        # Reply is 5 bytes: {x[0], x[1], x[2], t_sx[0], t_sx[1]}
+        # x[2:0] is a 24-bit 2's-complement measurement result
+        # t_sx[1:0] is a 16-bit unsigned T1 phase duration
+        # both values are transmitted in little-endian order
+        bytes_x = rly[0:3] + b'\0'
+        bytes_t_sx = rly[3:5]
+        x = struct.unpack('<I', bytes_x)[0]
+        t_sx = struct.unpack('<H', bytes_t_sx)[0]
+        # Fixup x sign, since it's a 24-bit 2's-complement
+        if x >= 0x800000:
+            x -= 0x1000000
+        # If T1 duration was 100 cycles, x is divided by 8, so we revert that
+        if dur == self.MEAS_2000MS:
+            x *= 8
+        # T_Sx value is expressed in 32-cycle units,
+        # while X value is expressed in 4-cycle units.
+        # We multiply T_Sx by 8 so it is aligned with X and is expressed
+        # in 4-cycle units.
+        t_sx *= 8
+        logger.debug(f'run_meas: ...measured X={x}, T_Sx={t_sx}, X/T_Sx={x/t_sx}')
+        return x, t_sx
+
     def test_comm(self):
-        self.xchg(bytes((0b10001000,)))
+        logger.info('test_comm: starting...')
+        r = random.Random()
+        r.seed(0)
+        seq_tx = list(range(256))
+        r.shuffle(seq_tx)
+        seq_tx = seq_tx[:255]  # test_comm echoes only 255 bytes
+        self.run_test(self.TEST_COMM)
+        seq_rx = self.xchg(bytes(seq_tx), len(seq_tx))
+        if len(seq_rx) != len(seq_tx) or tuple(seq_tx) != tuple(seq_rx):
+            logger.error(f'test_comm: ...echo test failed!\n Tx: {seq_tx!r}\n Rx: {seq_rx!r}')
+        else:
+            logger.info(f'test_comm: ...test passed')
 
 
-import time
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-c = Comm('/dev/ttyACM0')
+c = MetraD1639('/dev/ttyACM0')
 
 c.test_comm()
-# c.set_mode(20, False)
-# mode = 0
-# while True:
-#     mode = mode + 1
-#     mode = mode & 0x1F
-#     print(f'set_mode {mode}')
-#     c.set_mode(mode, False)
-#     time.sleep(0.2)
+
+c.set_mode(3, False)
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def meas():
+    for _ in range(4):
+        x, t_sx = c.run_meas(10)
+        logger.info(f'X={x:8d}, T_Sx={t_sx:7d}, X/T_Sx={x/t_sx:15f}')
+
+
+logger.info('------------ CAL ADC ZERO ------------')
+c.set_mode(26, False)
+meas()
+logger.info('------------ CAL ADC CAL+ ------------')
+c.set_mode(27, False)
+meas()
+logger.info('------------ CAL ADC CAL- ------------')
+c.set_mode(28, False)
+meas()
+logger.info('------------ OHMS 150R ------------')
+c.set_mode(7, False)
+meas()
+
